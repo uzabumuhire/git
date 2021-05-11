b2dc8ee094ae15d7d5e37d03a7c8eba51f776725
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-05-11 10:59:01 +0200

    Commit your staged changes

---
+++ committing-changes.ps1
@@ -0,0 +1,14 @@
+# Committing your changes
+
+# Remember that anything that is still unstaged — any files you have created or modified
+# that you haven’t run git add on since you edited them — won’t go into this commit. 
+# They will stay as modified files on your disk.
+
+# The commit records the snapshot you set up in your staging area. Anything you didn’t 
+# stage is still sitting there modified; you can do another commit to add it to your 
+# history. Every time you perform a commit, you’re recording a snapshot of your project 
+# that you can revert to or compare to later.
+
+# To commit your staged changes use git commit command. You can type your commit message 
+# inline with the commit command by specifying it after a -m flag.
+git commit -m "Your message ..."
