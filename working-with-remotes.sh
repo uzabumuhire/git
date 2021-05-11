77cccf9e4b2cf7150bfe3339fcc48e4d249a5229
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-05-11 18:39:58 +0200

    Push data to your remotes

--- working-with-remotes.sh
+++ working-with-remotes.sh
@@ -71,3 +71,19 @@ git config --global pull.rebase "false"
 # If you want to rebase when pulling: 
 
 git config --global pull.rebase "true"
+
+# Pushing to your remotes
+
+# When you have your project at a point that you want to share, you have to push it 
+# upstream. The command for this is simple: git push <remote> <branch>. If you want 
+# to push your master branch to your origin server (again, cloning generally sets up 
+# both of those names for you automatically), then you can run this to push any 
+# commits you’ve done back up to the server.
+
+git push origin master
+
+# This command works only if you cloned from a server to which you have write access 
+# and if nobody has pushed in the meantime. If you and someone else clone at the same 
+# time and they push upstream and then you push upstream, your push will rightly be 
+# rejected. You’ll have to fetch their work first and incorporate it into yours before 
+# you’ll be allowed to push.
