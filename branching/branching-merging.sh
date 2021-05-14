ddbf144bcd8718ff7ea06f5f6abc860ebc4eba8e
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-05-15 00:31:30 +0200

    Merge branches

--- branching/branching-merging.sh
+++ branching/branching-merging.sh
@@ -19,3 +19,36 @@ git checkout <newbranchname>
 # the last time you committed on that branch. It adds, removes, and modifies files
 # automatically to make sure your working copy is what the branch looked like on
 # your last commit to it.
+
+# Basic merging
+
+# In order to merge branch-B into branch-A, all you have to do is check out to
+# branch-A (the branch you swish to you wish to merge into) and then run the
+# git merge command.
+
+git checkout <branch-A>
+git merge <branch-B>
+
+# To phrase that another way, when you try to merge one commit with a commit that
+# can be reached by following the first commit’s history, Git simplifies things
+# by moving the pointer forward because there is no divergent work to merge
+# together — this is called a “fast-forward.”
+
+# If the commit on the branch you’re on isn’t a direct ancestor of the branch you’re
+# merging in, Git has to do some work. In this case, Git does a simple three-way
+# merge, using the two snapshots pointed to by the branch tips and the common
+# ancestor of the two.
+
+# Instead of just moving the branch pointer forward, Git creates a new snapshot
+# that results from this three-way merge and automatically creates a new commit
+# that points to it. This is referred to as a merge commit, and is special in that
+# it has more than one parent.
+
+# Now that your work is merged in, you have no further need for the  branch-B. You
+# can close the issue in your issue-tracking system, and delete the branch.
+
+# Delete the branch that just been merged into another branch, because you no
+# longer need it — the branch, you merged into, points at the same place. You can 
+# delete it with the -d option to git branch.
+
+git branch -d <branch-B>
