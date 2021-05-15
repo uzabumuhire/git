53db38afc2f049881f9f6cd3488c486e1c3634ff
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-05-15 12:44:39 +0200

    Understand Git three states

--- version-control.sh
+++ version-control.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # About Version Control System
 
 # A Version Control System (VCS) is a system that records changes to a file or set
@@ -53,3 +55,35 @@
 # if you regularly push your database to another repository. This makes using Git a
 # joy because we know we can experiment without the danger of severely screwing
 # things up.
+
+# Git three state
+
+# Git has three main states that your files can reside in: modified, staged, and 
+# committed:
+# - Modified means that you have changed the file but have not committed it to
+#   your database yet.
+# - Staged means that you have marked a modified file in its current version to
+#   go into your next commit snapshot.
+# - Committed means that the data is safely stored in your local database.
+
+# This leads us to the three main sections of a Git project: the working tree, the
+# staging area, and the Git directory:
+# - The working tree is a single checkout of one version of the project. These
+#   files are pulled out of the compressed database in the Git directory and
+#   placed on disk for you to use or modify.
+# - The staging area is a file, generally contained in your Git directory, that
+#   stores information about what will go into your next commit.
+# - The Git directory is where Git stores the metadata and object database for
+#   your project. This is the most important part of Git, and it is what is
+#   copied when you clone a repository from another computer.
+
+# The basic Git workflow goes something like this:
+# 1. You modify files in your working tree.
+# 2. You selectively stage just those changes you want to be part of your next
+#    commit, which adds only those changes to the staging area.
+# 3. You do a commit, which takes the files as they are in the staging area and
+#    stores that snapshot permanently to your Git directory.
+
+# If a particular version of a file is in the Git directory, it’s considered committed.
+# If it has been modified and was added to the staging area, it is staged. And if it
+# was changed since it was checked out but has not been staged, it is modified.
