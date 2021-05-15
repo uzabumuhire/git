ee8c7c6ef100db59bdd4da468af52f6cb252bd8f
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-05-15 11:55:40 +0200

    Understand Version Control System (VCS)

---
+++ version-control.sh
@@ -0,0 +1,10 @@
+# About Version Control System
+
+# A Version Control System (VCS) is a system that records changes to a file or set
+# of files over time so that you can recall specific versions later. VCS allows
+# you to revert selected files back to a previous state, revert the entire project
+# back to a previous state, compare changes over time, see who last modified
+# something that might be causing a problem, who introduced an issue and when, and
+# more. Using a VCS also generally means that if you screw things up or lose files,
+# you can easily recover. In addition, you get all this for very little overhead.
+
