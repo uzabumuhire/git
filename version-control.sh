0746b76e0c7fd103e1b5cb47c44caa938d65eed9
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-05-15 12:08:18 +0200

    Understand Git snapshots

--- version-control.sh
+++ version-control.sh
@@ -8,3 +8,17 @@
 # more. Using a VCS also generally means that if you screw things up or lose files,
 # you can easily recover. In addition, you get all this for very little overhead.
 
+# About Git
+
+# Git snapshots
+
+# Git thinks of its data more like a series of snapshots of a miniature filesystem. 
+# With Git, every time you commit, or save the state of your project, Git basically
+# takes a picture of what all your files look like at that moment and stores a
+# reference to that snapshot. To be efficient, if files have not changed, Git
+# doesn’t store the file again, just a link to the previous identical file it has
+# already stored. Git thinks about its data more like a stream of snapshots.
+
+# This makes Git more like a mini filesystem with some incredibly powerful tools
+# built on top of it, rather than simply a VCS.
+
