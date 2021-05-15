fdc3bbb4a6bc13c4c32b1028afcbd93243d03325
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-05-15 15:37:01 +0200

    Resolve merge conflict

--- branching/branching-merging.sh
+++ branching/branching-merging.sh
@@ -52,3 +52,51 @@ git merge <branch-B>
 # delete it with the -d option to git branch.
 
 git branch -d <branch-B>
+
+# Basic merge conflicts resolution
+
+# If you changed the same part of the same file differently in the two branches
+# you’re merging, Git won’t be able to merge them cleanly. This is called merge 
+# conflict. Git hasn’t automatically created a new merge commit. It has paused the
+# process while you resolve the conflict. If you want to see which files are
+# unmerged at any point after a merge conflict, you can run git status.
+
+git status
+
+# Anything that has merge conflicts and hasn’t been resolved is listed as unmerged.
+# Git adds standard conflict-resolution markers to the files that have conflicts,
+# so you can open them manually and resolve those conflicts.
+
+# The version in HEAD (your branch you had checked when you ran your merge command)
+# is the top part of that block (everything above the =======), while the version in
+# the branch, you want to merge in, is the bottom part. In order to resolve the 
+# conflict, you have to either choose one side or the other or merge the contents
+# yourself.
+
+# This resolution has a little of each section, and the <<<<<<<, =======, and >>>>>>>
+# lines have been completely removed. After you’ve resolved each of these sections
+# in each conflicted file, run git add on each file to mark it as resolved. Staging
+# the file marks it as resolved in Git.
+
+git add <file-in-current-branch>
+git add <file-in-another-branch>
+
+# If you want to use a graphical tool to resolve these issues, you can run git mergetool,
+# which fires up an appropriate visual merge tool and walks you through the conflicts.
+
+git mergetool
+
+# After you exit the merge tool, Git asks you if the merge was successful. If you tell the
+# script that it was, it stages the file to mark it as resolved for you. You can run git
+# status again to verify that all conflicts have been resolved.
+
+git status
+
+# If you’re happy with that, and you verify that everything that had conflicts has been
+# staged, you can type git commit to finalize the merge commit. git commit will use the
+# default commit message. If you think it would be helpful to others looking at this
+# merge in the future, you can modify this commit message with details about how you
+# resolved the merge and explain why you did the changes you made if these are not 
+# obvious.
+
+git commit 
